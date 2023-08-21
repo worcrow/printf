f357ff26f83415d63f093f3e44b3b478b6630cb0
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-21 16:04:06 +0100

    add files _printf.c function_used.c main.h print_format.c

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _printf - mimic printf function
+ * @format: string to be printd
+ * @...: variadic function
+ * Return: numbers of bytes printd
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i;
+	int count;
+	int temp;
+
+	i = 0;
+	count = 0;
+	va_start(ap, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0') || format[0] == '\0')
+		return (-1);
+	while (format && format[i] != '\0')
+	{
+		temp = 0;
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				count += print_char('%');
+				i++;
+			}
+			else
+			{
+				temp = print_format(ap, format[i + 1]);
+				if (temp == -1)
+					count += print_char(format[i]);
+				else
+				{
+					count += temp;
+					i++;
+				}
+			}
+		}
+		else
+			count += print_char(format[i]);
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
+
---
+++ function_used.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * print_char - print a charactere
+ * @c: charactere to be printed
+ * Return: numbers of bytes printed
+*/
+
+int print_char(int c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - print a string
+ * @s: string to be printed
+ * Return: numbers of bytes printed
+*/
+
+int print_string(char *s)
+{
+	int count;
+	char *temp;
+
+	if (s == NULL)
+		temp = "(null)";
+	else
+		temp = s;
+	count = 0;
+	while (temp[count] != '\0')
+	{
+		count += print_char(temp[count]);
+	}
+	return (count);
+}
+
+/**
+ * print_number - print a number based on their base
+ * @number: number to be printed
+ * @base: base to be printed in it
+ * Return: numbers of bytes printed
+*/
+
+int print_number(long number, int base)
+{
+	char *symbol = "0123456789abcdef";
+	int count;
+
+	if (number < 0)
+	{
+		print_char('-');
+		return (print_number(-number, base) + 1);
+	}
+	else if (number < base)
+		return (print_char(symbol[number]));
+
+	count = print_number(number / base, base);
+	return (print_char(symbol[number % base]) + count);
+}
+
